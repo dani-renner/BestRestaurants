c14722809e24fb4c76bbda8c7a3737c697c1bc5e
Dani Renner <danijrenner@gmail.com>
2021-03-10 14:22:47 -0800

    add code for Cuisine's views
    Co-authored by: Nick Lindau <nicholaithegreat@gmail.com>
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>

---
+++ BestRestaurants/Models/Cuisine.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace BestRestaurants.Models
+{
+  public class Cuisine
+  {
+    public int CuisineId { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<Restaurant> Restaurants { get; set;}
+    public Cuisine()
+    {
+      this.Restaurants = new HashSet<Restaurant>();
+    }
+  }
+}
